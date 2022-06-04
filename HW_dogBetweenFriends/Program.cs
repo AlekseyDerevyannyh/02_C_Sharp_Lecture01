83dffcc9d44bc6854082b942605bfcdbc083ad03
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-04 19:27:39 +0300

    add HomeWork Dog Between Friends

---
+++ HW_dogBetweenFriends/Program.cs
@@ -0,0 +1,24 @@
+﻿int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int distance = 10000;
+int distanceMin = 10;
+double timeToMeet = 0;
+
+int friend = 2;
+int count = 0;
+double distanceBetweenFriends = distance;
+
+while (distanceBetweenFriends > distanceMin) {
+    if (friend == 2) {
+        timeToMeet = distanceBetweenFriends / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    } else {
+        timeToMeet = distanceBetweenFriends / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    distanceBetweenFriends -= timeToMeet * (firstFriendSpeed + secondFriendSpeed);
+    count ++;
+}
+Console.WriteLine(count);
+ 
