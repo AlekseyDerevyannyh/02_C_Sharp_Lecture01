6ce2c61866678cb19d2727659860320c426e3231
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-04 17:01:39 +0300

    add Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")   Console.WriteLine("Ура, это же МАША!");
+else                                Console.WriteLine("Привет, {0}", username);
